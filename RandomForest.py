6cb0254deca29f430cf362015dd003715bcf2aa8
atarsander <aleksander.szymczyk.stud@pw.edu.pl>
2025-01-15 23:20:37 +0100

    Template files for randomf forest and treeNode

---
+++ RandomForest.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+class RandomForest:
+    def __init__(self, dataset, n_estimators):
+        self.n_estimators = n_estimators
+
+    def bootstraping(self):
+        pass
---
+++ TreeNode.py
@@ -0,0 +1,4 @@
+class TreeNode:
+    def __init__(self, feature, value):
+        self.feature = feature
+        self.value = value
